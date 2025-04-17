f1da5209fc21cae63d7d1570db399573df0b1a25
yashksaini-coder <ys3853428@gmail.com>
2025-04-17 21:02:09 +0530

    (feat): add schema definition for users and roadmaps tables

---
+++ convex/schema.ts
@@ -0,0 +1,48 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+import { scheduler } from "timers/promises";
+
+export default defineSchema({
+    users: defineTable({
+        name: v.string(),
+        email: v.string(),
+        image: v.optional(v.string()),
+        clerkId: v.string(),
+    }).index("byClerkId", ["clerkId"]),
+
+    roadmaps: defineTable({
+        userId: v.id("users"),
+        name: v.string(),
+        roadmapPlan: v.object({
+            goal: v.string(),              // Why they're learning to code
+            skill: v.union(
+                v.literal("beginner"),
+                v.literal("intermediate"),
+                v.literal("advanced")
+            ),
+            schedule: v.union(
+                v.literal("<2h"),
+                v.literal("2-5h"),
+                v.literal("5-10h"),
+                v.literal("10+h")
+            ),
+            interest: v.array(v.string()), // Areas like web, mobile, AI, etc.
+            deadline: v.optional(v.string()), // e.g. "3 months", ISO date, etc.
+            constraints: v.optional(v.array(v.string())), // Free/Paid, Accessibility, etc.
+            preferences: v.optional(
+                v.object({
+                    style: v.array(v.string()), // e.g. video, project-based
+                    language: v.optional(v.string()), // preferred programming language
+                })
+            ),
+            status: v.union(
+                v.literal("new"),
+                v.literal("in_progress"),
+                v.literal("completed")
+            ),
+        }),
+        isActive: v.boolean(),
+    }).index("byUserId", ["userId"]).index("byActive", ["isActive"]),
+});
+
+
