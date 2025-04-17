cbf991f55af85f4a487086e633678dc3886e3f89
yashksaini-coder <ys3853428@gmail.com>
2025-04-17 18:53:07 +0530

    (auth): Setup clerk auth & middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client"
 
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 export default function page() {
   return (
     <div>
-      
+      Home Page
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
