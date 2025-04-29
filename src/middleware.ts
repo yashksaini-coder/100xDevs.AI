ebe176887db15925522082dd1972aeba10740435
yashksaini-coder <ys3853428@gmail.com>
2025-04-29 19:52:56 +0530

    (fix): Update protected route for roadmap generation in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/generate-program", "/profile"]);
+const isProtectedRoute = createRouteMatcher(["/generate-roadmap", "/profile"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
